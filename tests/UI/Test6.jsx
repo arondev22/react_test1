9314febaec3d14db7b206482dc7d42cb2d3ce795
Ron <aronandrada22@gmail.com>
2024-07-04 18:19:05 +0800

    update desktop responsiveness

--- tests/UI/Test6.jsx
+++ tests/UI/Test6.jsx
@@ -34,6 +34,7 @@ import DownloadIcon from '@mui/icons-material/Download';
 import cashless from '../../public/images/cashless.png';
 import transpo from '../../public/images/transpo.png';
 import safety from '../../public/images/safety.png';
+import pickup from '../../public/images/pickup.png';
 
 export default function Test6Screen() {
   return (
@@ -41,109 +42,151 @@ export default function Test6Screen() {
       sx={{paddingLeft: 0, paddingRight: 0, paddingBottom: '30px', paddingTop: '71px'}}
     >
       <Box 
-        sx={{ marginRight: '12px', marginLeft: '12px' }}
+        sx={{ marginRight: '12px', marginLeft: '12px', justifyContent: 'center' }}
       >
 
         <Box
-          sx={{ backgroundColor: '#EAEDFB', display: 'grid', justifyContent: 'center', borderRadius: '24px', padding: '12px' }}
+          sx={{
+            '@media (min-width: 640px)': {
+                display: 'flex',
+                flexDirection: 'row-reverse'
+            }
+          }}
         >
           <Box
-            component="img"
-            src={yay}
-          />
-        </Box>
-
-        <Box
-          sx={{ width: '100%', paddingRight: '10%', paddingLeft: '10%' }}
-        >
-          <Typography
-            sx={{ marginTop: '100px', marginBottom: '50px', fontFamily: 'Archivo', color: '#334155' }}
+            sx={{ backgroundColor: '#EAEDFB', display: 'grid', justifyContent: 'center', borderRadius: '24px', padding: '12px', width: '100%' }}
           >
-            Drive the nation to a more <Typography component="span" sx={{fontWeight: '700'}}>seamless, convenient mobility</Typography> and <Typography component="span" sx={{fontWeight: '700'}}>efficient logistics</Typography>.
-          </Typography>
-          <AppButton 
-            buttonName='Get The App Now'
-            fullWidth
-            startIcon={<DownloadIcon sx={{ width: 36, height: 36 }} />}
-            sx={{ fontSize: '18px', height: '75px', backgroundColor: '#182C75', marginBottom: '32px', fontFamily: 'Inter', textTransform: 'capitalize' }}
-          />
-          <AppButton 
-            buttonName='Admin Login'
-            variant="outlined"
-            fullWidth
-            startIcon={<ExitToAppIcon sx={{ width: 36, height: 36 }} />}
-            sx={{ fontSize: '18px', height: '75px', borderColor: '#182C75', color: '#182C75', fontFamily: 'Inter', textTransform: 'capitalize' }}
-          />
-        </Box>
+            <Box
+              component="img"
+              src={yay}
+            />
+          </Box>
 
-        <Box
-          sx={{ backgroundColor: '#F1F5F9', padding: '70px 20px 60px 30px', borderRadius: '24px', marginTop: '80px' }}
-        >
-          <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155', marginBottom: '20px' }}>
-            Pick Up and Delivery
-          </Typography>
-          <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B' }}>
-            Taking good control of the delivery process in order to make sure that the parcel is delivered to you on time.
-          </Typography>
-        </Box>
-
-        <Box
-          sx={{ backgroundColor: '#F1F5F9', padding: '70px 20px 60px 20px', borderRadius: '24px', marginTop: '24px', display: 'flex', flexDirection: 'row' }}
-        >
           <Box
-            component="img"
-            src={cashless}
-            sx={{ width: '102.36px', marginRight: '12px' }}
-          />
-          <Box
-            sx={{
-              alignContent: 'center'
-            }}
+            sx={{ width: '100%', paddingRight: '10%', paddingLeft: '10%' }}
           >
-            <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155', marginBottom: '20px' }}>
-              Cashless Payments
+            <Typography
+              sx={{ marginTop: '100px', marginBottom: '50px', fontFamily: 'Archivo', color: '#334155', '@media (min-width: 640px)': { fontSize: '24px' } }}
+            >
+              Drive the nation to a more <Typography component="span" sx={{fontWeight: '700', '@media (min-width: 640px)': { fontSize: '24px' }}}>seamless, convenient mobility</Typography> and <Typography component="span" sx={{fontWeight: '700', '@media (min-width: 640px)': { fontSize: '24px' }}}>efficient logistics</Typography>.
             </Typography>
-            <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B' }}>
-              Hassle-free experience by using a cashless payment through Gcash.
+            <Box
+              sx={{
+                '@media (min-width: 640px)': {
+                    display: 'flex',
+                    gap: '15px'
+                }
+              }}
+            >
+              <AppButton 
+                buttonName='Get The App Now'
+                fullWidth
+                startIcon={<DownloadIcon sx={{ width: 36, height: 36, '@media (min-width: 640px)': { width: 24, height: 24 } }} />}
+                sx={{ fontSize: '18px', height: '75px', backgroundColor: '#182C75', marginBottom: '32px', fontFamily: 'Inter', textTransform: 'capitalize', '@media (min-width: 640px)': { fontSize: '14px', height: '50px' } }}
+              />
+              <AppButton 
+                buttonName='Admin Login'
+                variant="outlined"
+                fullWidth
+                startIcon={<ExitToAppIcon sx={{ width: 36, height: 36, '@media (min-width: 640px)': { width: 24, height: 24 } }} />}
+                sx={{ fontSize: '18px', height: '75px', borderColor: '#182C75', color: '#182C75', fontFamily: 'Inter', textTransform: 'capitalize', '@media (min-width: 640px)': { fontSize: '14px', height: '50px' } }}
+              />
+            </Box>
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            marginTop: '80px',
+            '@media (min-width: 640px)': {
+                display: 'flex',
+                gap: '25px'
+            }
+          }}
+        >
+          <Box
+            sx={{ backgroundColor: '#CCD5F5', padding: '20', borderRadius: '24px', '@media (max-width: 640px)': { display: 'none' } }}
+          >
+            <Box
+              component="img"
+              src={pickup}
+              sx={{ width: '546px' }}
+            />
+          </Box>
+
+          <Box
+            sx={{ backgroundColor: '#F1F5F9', padding: '70px 20px 60px 30px', borderRadius: '24px', alignContent: 'center' }}
+          >
+            <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155', marginBottom: '20px', '@media (min-width: 640px)': { fontSize: '45px' } }}>
+              Pick Up and Delivery
+            </Typography>
+            <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B', '@media (min-width: 640px)': { fontSize: '15px' } }}>
+              Taking good control of the delivery process in order to make sure that the parcel is delivered to you on time.
             </Typography>
           </Box>
         </Box>
 
         <Box
-          sx={{ backgroundImage: 'linear-gradient(-180deg, #fff 60%, #304D23 134%)', padding: '40px 20px 0px 20px', borderRadius: '24px', marginTop: '24px', display: 'ruby-text' }}
+          sx={{
+            '@media (min-width: 640px)': {
+              display: 'flex',
+              gap: '25px'
+            }
+          }}
         >
-          <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155', marginBottom: '20px' }}>
-            Transportation
-          </Typography>
-          <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B', textAlign: 'center' }}>
-            Provide a convenient and secure local transportation
-          </Typography>
           <Box
-            component="img"
-            src={transpo}
-            sx={{
-              width: '102.36px',
-              marginTop: '15px'
-            }}
-          />
+            sx={{ backgroundColor: '#F1F5F9', padding: '70px 20px 60px 20px', borderRadius: '24px', marginTop: '24px', display: 'flex', flexDirection: 'row', width: '100%' }}
+          >
+            <Box
+              component="img"
+              src={cashless}
+              sx={{ width: '102.36px', marginRight: '12px', '@media (min-width: 640px)': { width: '283px' } }}
+            />
+            <Box
+              sx={{
+                alignContent: 'center'
+              }}
+            >
+              <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155', marginBottom: '20px', '@media (min-width: 640px)': { fontSize: '50px', lineHeight: '1.1' } }}>
+                Cashless Payments
+              </Typography>
+              <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B', '@media (min-width: 640px)': { fontSize: '20px' } }}>
+                Hassle-free experience by using a cashless payment through Gcash.
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{ backgroundImage: 'linear-gradient(-180deg, #fff 60%, #304D23 134%)', width: '100%', padding: '40px 20px 0px 20px', borderRadius: '24px', marginTop: '24px', display: 'ruby-text', '@media (min-width: 640px)': { display: 'grid', alignItems: 'center', justifyItems: 'center' } }}
+          >
+            <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155', marginBottom: '20px', '@media (min-width: 640px)': { fontSize: '40px' } }}>
+              Transportation
+            </Typography>
+            <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B', textAlign: 'center', '@media (min-width: 640px)': { fontSize: '18px' } }}>
+              Provide a convenient and secure local transportation
+            </Typography>
+            <Box
+              component="img"
+              src={transpo}
+              sx={{width: '102.36px',marginTop: '15px', '@media (min-width: 640px)': { width: '323px' }}}
+            />
+          </Box>
         </Box>
 
         <Box
-          sx={{ backgroundColor: '#CCD5F5', padding: '30px 20px 50px 20px', borderRadius: '24px', marginTop: '24px' }}
+          sx={{ backgroundColor: '#CCD5F5', padding: '30px 20px 50px 20px', borderRadius: '24px', marginTop: '24px', '@media (min-width: 640px)': { display: 'flex' } }}
         >
           <Box
             component="img"
             src={safety}
           />
           <Box
-            sx={{
-              margin: '15px'
-            }}
+            sx={{margin: '15px', '@media (min-width: 640px)': { alignContent: 'center' }}}
           >
-            <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155' }}>
+            <Typography sx={{ fontFamily: 'Inter', fontSize: '24px', fontWeight: '700', color: '#334155', '@media (min-width: 640px)': { fontSize: '35px' } }}>
               Customer Safety
             </Typography>
-            <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B' }}>
+            <Typography sx={{ fontFamily: 'Inter', fontSize: '12px', color: '#64748B', '@media (min-width: 640px)': { fontSize: '17px' } }}>
               GPS tracking ensures that drivers are always on the right track
             </Typography>
           </Box>
